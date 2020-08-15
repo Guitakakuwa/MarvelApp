ef9f849717addf49f1afec2fa9f0b7efbe8f7feb
Guilherme Takakuwa <guilherme.takakuwa@vee.digital>
2020-08-15 17:03:04 -0300

    added REST call to marvel API and linked storyboard to ListCharactersTableViewController

--- MarvelApp/ListCharactersTableViewController.swift
+++ MarvelApp/ListCharactersTableViewController.swift
@@ -8,37 +8,63 @@
 
 import UIKit
 import CommonCrypto
+import Alamofire
 
 class ListCharactersTableViewController: UITableViewController {
-     let MARVEL_PUBLIC_KEY: String = "26a2b99fb1cde83f61e79d53c65803ba"
-     let MARVEL_PRIVATE_KEY: String = "c4cc055e3f7668521cf813b4489d2ea791a17b8a"
-    
+    let MARVEL_PUBLIC_KEY: String = "26a2b99fb1cde83f61e79d53c65803ba"
+    let MARVEL_PRIVATE_KEY: String = "c4cc055e3f7668521cf813b4489d2ea791a17b8a"
+    let BASE_URL: String = "http://gateway.marvel.com/v1/public/"
+    var offsetNumber = 0
+    var limitNumber = 20
     override func viewDidLoad() {
         super.viewDidLoad()
-        let timestamp = "1"
-        self.createAPIKey(timestamp,MARVEL_PRIVATE_KEY,MARVEL_PUBLIC_KEY)
+        self.getCharacter()
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
-
-
-    func createAPIKey(_ timestamp:String ,_ privateKey:String,_ publicKey:String) -> (String){
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterInformationCell", for: indexPath)
+        return cell
+    }
+    
+    
+    func createAPIKey(timestamp timestamp:String ,privateKey privateKey:String,publicKey publicKey:String) -> (String){
         let initialStringHash = timestamp + privateKey + publicKey
         let finalHash =  initialStringHash.md5
         print(finalHash)
         return finalHash
     }
-
+    
+    func getCharacter(){
+        let APIhash = self.createAPIKey(timestamp:"1",privateKey:MARVEL_PRIVATE_KEY,publicKey:MARVEL_PUBLIC_KEY)
+        let charactersEndpoint = "characters?"
+        let ts = "ts=1"
+        let publicKey = "&apikey="+MARVEL_PUBLIC_KEY
+        let hash = "&hash="+APIhash
+        let limit = "&limit="+String(limitNumber)
+        let offset = "&offset="+String(offsetNumber)
+        
+        AF.request(BASE_URL+charactersEndpoint+ts+publicKey+hash+limit+offset).response { response in
+            self.offsetNumber = self.offsetNumber + self.limitNumber
+            debugPrint(response)
+        }
+    }
 }
 
 extension String {
@@ -52,4 +78,4 @@ extension String {
         return hash.map { String(format: "%02x", $0) }.joined()
     }
 }
-    
+
