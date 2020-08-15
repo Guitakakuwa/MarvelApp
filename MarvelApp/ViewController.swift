259b91c992436aae617efa517ab9d190d7d9f8be
Guilherme Takakuwa <guilherme.takakuwa@vee.digital>
2020-08-15 14:52:37 -0300

    Strcuture commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Guilherme Takakuwa on 15/08/20.
+//  Copyright © 2020 Guilherme Takakuwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
